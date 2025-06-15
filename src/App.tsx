7e88d2c511eb81f140a12527e9f02484f8dce910
mhserrao <maizy_serrao@outlook.com>
2025-06-14 20:57:17 -0400

    Updated the router to come from BrowserRouter to support the proxy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <BrowserRouter basename="/breadcrumb">
       <AppRoutes />
-    </Router>
+    </BrowserRouter>
   );
 };
 
